b2680d083c42bed137eb6dea47e016dfd9eb25cc
mechworrior <ni.tony.ym@gmail.com>
2025-12-03 13:38:59 +0900

    Day 02

---
+++ Day_02/Part_01/solution.py
@@ -0,0 +1,26 @@
+# Current folder
+from pathlib import Path as path
+
+day_folder = path(__file__).parent.parent
+
+with open(day_folder / "input.txt") as file:
+    line = file.readline()
+
+def is_bad_number(product_id):
+    product_id_str =str(product_id)
+    len_product_id = len(product_id_str)
+
+    if len_product_id % 2 == 0:
+        if product_id_str[:len(product_id_str)//2] == product_id_str[len(product_id_str)//2:]:
+            return True
+        
+
+ranges = line.strip().split(",")
+bad_numbers = set()
+for range_ in ranges: 
+    start, end = map(int, range_.split("-"))
+    for number in range(start, end + 1):
+        if is_bad_number(number):
+            bad_numbers.add(number)
+
+print(sum(bad_numbers))
